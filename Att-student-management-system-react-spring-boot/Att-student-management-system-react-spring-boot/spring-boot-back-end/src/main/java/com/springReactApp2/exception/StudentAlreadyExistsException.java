1552e1d65a0a7e42c0b1f1b92f7763c2519ef42f
Theprofessorrr <95654155+Theprofessorrr@users.noreply.github.com>
2024-12-06 00:33:28 +0200

    Delete Att-student-management-system-react-spring-boot/Att-student-management-system-react-spring-boot directory

--- Att-student-management-system-react-spring-boot/Att-student-management-system-react-spring-boot/spring-boot-back-end/src/main/java/com/springReactApp2/exception/StudentAlreadyExistsException.java
+++
@@ -1,7 +0,0 @@
-package com.springReactApp2.exception;
-
-public class StudentAlreadyExistsException extends RuntimeException {
-    public StudentAlreadyExistsException(String message) {
-        super(message);
-    }
-}
--- Att-student-management-system-react-spring-boot/Att-student-management-system-react-spring-boot/spring-boot-back-end/src/main/java/com/springReactApp2/exception/StudentNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.springReactApp2.exception;
-
-public class StudentNotFoundException extends RuntimeException {
-    public StudentNotFoundException(String message) {
-        super(message);
-    }
-}
